b8bc9b49f810d21f72164c7310f5063484cf21dc
abielVivas <abimaestro.vivas@gmail.com>
2024-04-20 11:13:59 -0400

    falta acomodar bien las barras

--- graficoSvg/grafico.js
+++ graficoSvg/grafico.js
@@ -1,54 +1,60 @@
+// Definición de la función svg para contar la repetición de letras
+function svg(array) {
+    var contador = [
+        ["A", 0],
+        ["B", 0],
+        ["C", 0],
+        ["D", 0],
+        ["E", 0],
+        ["F", 0]
+    ];
 
-
-//esta parte es la encargada de contar la repeticion de las letras
-var contador = [
-    [ "A", 0 ],
-    [ "B", 0 ],
-    [ "C", 0 ],
-    [ "D", 0 ],
-    [ "E", 0 ],
-    [ "F", 0 ] 
-  ];
-
-  
-//esta funcion va a contar la repeticion de las letras
-
-function svg (array){
-    for(var i=0; i<=array.length; i++){
-        //console.log(`i es igual a ${i}`)
-        for( j=0; j<contador.length; j++){
-            //console.log(`j es igual a ${j}`)
-            if(array[i] == contador[j][0]){
-                contador[j][1]++
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < contador.length; j++) {
+            if (array[i] == contador[j][0]) {
+                contador[j][1]++;
             }
         }
     }
+
+    return contador;
 }
-svg(["B", "A", "A", "A", "B", "C", "D", "C", "C", "B", "B", "A", "A", "E", "E", "F"]);
-console.log (contador)
-//hasta esta parte voy biennnnn
 
+// Llamada a la función svg con un arreglo de letras
+var contador = svg(["B", "A", "A", "A", "B", "C", "D", "C", "C", "B", "B", "A", "A", "E", "E", "F"]);
 
+console.log(contador);
 
-
+// Configuración inicial para el gráfico SVG
+var anchoDelLienzo = 500;
+var cantidadDeSeparacion = Math.round(anchoDelLienzo / (contador.length + 1));
 var svgNS = "http://www.w3.org/2000/svg";
+var rectanguloEnX = 80;
+var anchoDelRectangulo = anchoDelLienzo / (contador.length + 8);
 
-var rectanguloEnX = 80 
-var anchoDelRectangulo = 50
+// Creación y configuración de la línea horizontal
+var linea = document.createElementNS(svgNS, "line");
+linea.setAttribute("x1", "50");
+linea.setAttribute("y1", "550");
+linea.setAttribute("x2", rectanguloEnX + cantidadDeSeparacion * contador.length - cantidadDeSeparacion / 2);
+linea.setAttribute("y2", "550");
+linea.setAttribute("stroke", "black");
+linea.setAttribute("stroke-width", "2");
 
-var cantidad = 600 / (contador.length + 1)
+// Agregando la línea al contenedor SVG
+var svgContainer = document.getElementById("svgContainer");
+svgContainer.appendChild(linea);
 
-for(i=0; i<contador.length; i++){
-    
-    //aqui estan las variables de las posiciones de los rectangulos
-    var rectanguloEnY = 550 - (contador[i][1] * 100)
-    
-    var largoDelRectangulo = 550 - rectanguloEnY
+svgContainer.setAttribute("width", anchoDelLienzo);
+
+// Dibujando los rectángulos y etiquetas
+for (var i = 0; i < contador.length; i++) {
+    var rectanguloEnY = 550 - (contador[i][1] * 100);
+    var largoDelRectangulo = 550 - rectanguloEnY;
     
     var rect = document.createElementNS(svgNS, "rect");
     var letra = document.createElementNS(svgNS, "text");
-    
-    // Establecer atributos del rectángulo (coordenadas, tamaño, color, etc.)
+
     rect.setAttribute("x", rectanguloEnX);
     rect.setAttribute("y", rectanguloEnY);
     rect.setAttribute("width", anchoDelRectangulo);
@@ -57,18 +63,14 @@ for(i=0; i<contador.length; i++){
 
     letra.setAttribute("x", rectanguloEnX + 20);
     letra.setAttribute("y", 585);
-    letra.setAttribute("font-size", 20)
-    letra.setAttribute("font-family","arial")
+    letra.setAttribute("font-size", 20);
+    letra.setAttribute("font-family", "arial");
 
-    var texto = document.createTextNode(contador[i][0])
+    var texto = document.createTextNode(contador[i][0]);
 
-    // Agregar el rectángulo al documento SVG (puede ser un elemento <svg> o <g>)
-    var svgContainer = document.getElementById("svgContainer"); // Suponiendo que tienes un elemento con el id "svgContainer"
+    rectanguloEnX = rectanguloEnX + cantidadDeSeparacion;
+
     svgContainer.appendChild(rect);
     letra.appendChild(texto);
-
     svgContainer.appendChild(letra);
-
-
-    rectanguloEnX = rectanguloEnX + cantidad
 }
