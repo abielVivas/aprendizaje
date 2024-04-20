33037189d10957a215421f3a91027aac36c401bf
abielVivas <abimaestro.vivas@gmail.com>
2024-04-20 13:08:39 -0400

    ya casito

--- graficoSvg/grafico.js
+++ graficoSvg/grafico.js
@@ -25,32 +25,54 @@ var contador = svg(["B", "A", "A", "A", "B", "C", "D", "C", "C", "B", "B", "A", 
 
 console.log(contador);
 
+
+
+
+
+
+
 // Configuración inicial para el gráfico SVG
-var anchoDelLienzo = 500;
-var cantidadDeSeparacion = Math.round(anchoDelLienzo / (contador.length + 1));
+var anchoDelLienzo = 300;
+var altoDelLienzo = anchoDelLienzo
+var cantidadDeSeparacion = Math.round(anchoDelLienzo / (contador.length + 2));
+var rectanguloEnX = anchoDelLienzo * 0.15
+var anchoDelRectangulo = anchoDelLienzo / (contador.length + 9);
 var svgNS = "http://www.w3.org/2000/svg";
-var rectanguloEnX = 80;
-var anchoDelRectangulo = anchoDelLienzo / (contador.length + 8);
 
 // Creación y configuración de la línea horizontal
 var linea = document.createElementNS(svgNS, "line");
-linea.setAttribute("x1", "50");
-linea.setAttribute("y1", "550");
-linea.setAttribute("x2", rectanguloEnX + cantidadDeSeparacion * contador.length - cantidadDeSeparacion / 2);
-linea.setAttribute("y2", "550");
+linea.setAttribute("x1", anchoDelLienzo * 0.1);
+linea.setAttribute("y1", altoDelLienzo * 0.9);
+linea.setAttribute("x2", anchoDelLienzo * 0.9);
+linea.setAttribute("y2", altoDelLienzo * 0.9);
 linea.setAttribute("stroke", "black");
 linea.setAttribute("stroke-width", "2");
+
+
+
+var linea2 = document.createElementNS(svgNS, "line");
+linea2.setAttribute("x1", anchoDelLienzo * 0.1);
+linea2.setAttribute("y1", altoDelLienzo * 0.1); // Cambiada a la misma posición vertical
+linea2.setAttribute("x2", anchoDelLienzo * 0.1);
+linea2.setAttribute("y2", altoDelLienzo * 0.9); // Cambiada a la misma posición vertical
+linea2.setAttribute("stroke", "black");
+linea2.setAttribute("stroke-width", "2");
+
 
 // Agregando la línea al contenedor SVG
 var svgContainer = document.getElementById("svgContainer");
 svgContainer.appendChild(linea);
+svgContainer.appendChild(linea2);
+
 
 svgContainer.setAttribute("width", anchoDelLienzo);
+svgContainer.setAttribute("height", altoDelLienzo);
+
 
 // Dibujando los rectángulos y etiquetas
 for (var i = 0; i < contador.length; i++) {
-    var rectanguloEnY = 550 - (contador[i][1] * 100);
-    var largoDelRectangulo = 550 - rectanguloEnY;
+    var rectanguloEnY = (altoDelLienzo * 0.9) - (contador[i][1] * 100);
+    var largoDelRectangulo = (altoDelLienzo *0.9) - rectanguloEnY;
     
     var rect = document.createElementNS(svgNS, "rect");
     var letra = document.createElementNS(svgNS, "text");
@@ -61,9 +83,9 @@ for (var i = 0; i < contador.length; i++) {
     rect.setAttribute("height", largoDelRectangulo);
     rect.setAttribute("fill", "blue");
 
-    letra.setAttribute("x", rectanguloEnX + 20);
-    letra.setAttribute("y", 585);
-    letra.setAttribute("font-size", 20);
+    letra.setAttribute("x", rectanguloEnX + (anchoDelRectangulo / 3));
+    letra.setAttribute("y", altoDelLienzo * 0.95);
+    letra.setAttribute("font-size", (anchoDelRectangulo / 2));
     letra.setAttribute("font-family", "arial");
 
     var texto = document.createTextNode(contador[i][0]);
