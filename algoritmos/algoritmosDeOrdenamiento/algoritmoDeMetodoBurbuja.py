1dfe1364302823ee2089a6ce03cd3b81f8a8b2e1
abielVivas <abimaestro.vivas@gmail.com>
2024-04-24 12:49:17 -0400

    se le agrego un else para cuando el numero es menor se le sume 1 a i

--- algoritmos/algoritmosDeOrdenamiento/algoritmoDeMetodoBurbuja.py
+++ algoritmos/algoritmosDeOrdenamiento/algoritmoDeMetodoBurbuja.py
@@ -5,6 +5,7 @@ while i < len(lista) -1:
         contenedor = lista[i]
         lista[i] = lista[i+1]
         lista[i+1] = contenedor
-        i = -1
-    i = i + 1
+        i = 0
+    else:
+        i = i + 1
 print(lista)
